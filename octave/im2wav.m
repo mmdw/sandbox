7c067108b6d9ee6fe605a106b68ff49936819ec0
mmdw <mmdw631@gmail.com>
2012-03-26 23:40:47 +0400

    octave im2wav

---
+++ octave/im2wav.m
@@ -0,0 +1,17 @@
+#! /usr/bin/octave -qf
+
+I = imread("inp.png");
+x = size(I)(1);
+y = size(I)(2);
+
+res = zeros(x * y, 1);
+
+sample_rate = 44100;
+
+I = 1.0 - I(:, :, 1)/255;
+I = flipud(I);
+R = ifft(I, 1024);
+
+res = reshape(R, size(R)(1) * size(R)(2), 1);
+
+wavwrite(res, sample_rate, 16, "out.wav");
